4457f0d53f3e6b058d2ab392304c84be38bb0cd1
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2024-12-21 22:26:19 +0000

    Basic MongoDB setup and connection

--- server/server.js
+++ server/server.js
@@ -2,9 +2,19 @@
 // for new way add "type": "module", in package.json
 
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+// console.log(process.env.MONGO_URI);
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
 app.listen(5000, () => {
+  connectDB();
   console.log("Server listening on port 5000!");
 });
