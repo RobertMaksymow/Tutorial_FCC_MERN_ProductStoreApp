7e2f15f4e459df0099c48b4f153432ca20b96cc3
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2024-12-23 18:41:12 +0000

    Building API-productSchema for Mongo. Time 28min

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./modelsDB/productModel.js";
 
 dotenv.config();
 // console.log(process.env.MONGO_URI);
@@ -12,6 +13,26 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
+});
+
+app.post("/products", async (req, res) => {
+  const product = req.body; //user will send this data to the server
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(5000, () => {
