b8f24dec53cb3e5ee402e172b5d410f5553cf671
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-01-11 22:57:59 +0000

    Adding get request. 38min

--- server/server.js
+++ server/server.js
@@ -18,6 +18,21 @@ app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
+app.get("/api_v1/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error in fetching products", error.message);
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Server Error while getting all products",
+      });
+  }
+});
+
 app.post("/api_v1/products", async (req, res) => {
   const product = req.body; //user will send this data to the server
 
@@ -35,6 +50,18 @@ app.post("/api_v1/products", async (req, res) => {
   } catch (error) {
     console.error("Error in Create product: ", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api_v1/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product not found" });
   }
 });
 
