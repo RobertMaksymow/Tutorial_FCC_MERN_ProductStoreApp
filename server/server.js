ad8740914b44a4d684d33d15fd04bbe88bd8e1c1
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-01-26 18:20:49 +0000

    Pushing files again

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 
@@ -12,14 +14,25 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const __dirname = path.resolve();
+
 //Middleware
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.get("/", (req, res) => {
-  res.send("Welcome!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome!");
+// });
 
 app.use("/api_v1/products", productRoutes);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
